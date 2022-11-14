bd342cca01481690897d9ca5cf74c453bfb0e2d4
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2022-11-13 18:14:15 -0800

    Limit to Rack 3 only.

--- test/rack/conform/websocket.rb
+++ test/rack/conform/websocket.rb
@@ -9,14 +9,18 @@ include ClientContext
 require 'async/websocket/client'
 let(:websocket_client) {Async::WebSocket::Client.open(endpoint)}
 
-it 'can establish a websocket connection' do
-	connection = websocket_client.connect(endpoint.authority, "/websocket/echo")
-	
-	begin
-		connection.send_text("Hello World")
-		message = connection.read
-		expect(message).to be == "Hello World"
-	ensure
-		connection.close
+require 'rack'
+
+if Rack::RELEASE > "3.0"
+	it 'can establish a websocket connection' do
+		connection = websocket_client.connect(endpoint.authority, "/websocket/echo")
+		
+		begin
+			connection.send_text("Hello World")
+			message = connection.read
+			expect(message).to be == "Hello World"
+		ensure
+			connection.close
+		end
 	end
 end
